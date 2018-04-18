79a569432c539e37468c42ad5ccd36293d663d2b
Dmitrij Fedorenko <c0va23@gmail.com>
2018-04-18 10:10:50 +0300

    Init
    Configure Webpack + TypeScript + React

---
+++ webpack.config.js
@@ -0,0 +1,56 @@
+const webpack = require('webpack')
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+/*
+ * We've enabled UglifyJSPlugin for you! This minifies your app
+ * in order to load faster and run less javascript.
+ *
+ * https://github.com/webpack-contrib/uglifyjs-webpack-plugin
+ *
+ */
+
+const DEFAULT_NODE_ENV = 'production'
+const NODE_ENV = process.env['NODE_ENV'] || DEFAULT_NODE_ENV
+
+module.exports = {
+  entry: './src/index.tsx',
+
+  output: {
+    filename: '[name].[chunkhash].bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+
+  mode: NODE_ENV,
+
+  devtool: "source-map",
+
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        enforce: "pre",
+        test: /\.js$/,
+        loader: "source-map-loader",
+      },
+    ],
+  },
+
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.json'],
+  },
+
+  optimization: {
+    minimize: NODE_ENV == DEFAULT_NODE_ENV,
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'src/index.html',
+    }),
+  ],
+}
