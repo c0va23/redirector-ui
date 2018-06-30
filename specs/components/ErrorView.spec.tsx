43f1b390e68f30d6c4be12f92faf0c2efcb93a61
Dmitrij Fedorenko <c0va23@gmail.com>
2018-06-30 19:11:51 +0300

    Add ErrorView components

---
+++ specs/components/ErrorView.spec.tsx
@@ -0,0 +1,73 @@
+import * as React from 'react'
+import ErrorView from '../../src/components/ErrorView'
+
+import { ReactWrapper, mount } from 'enzyme'
+import { lorem } from 'faker'
+
+describe('ErrorView', () => {
+  let bodyText: string
+  let status: number
+  let statusText: string
+  let response: {
+    status: number,
+    statusText: string,
+    text: jest.Mock,
+  }
+  let errorView: ReactWrapper
+
+  beforeEach(() => {
+    bodyText = lorem.sentence()
+    status = 400 + Math.round(100 * Math.random())
+    statusText = lorem.sentence(2)
+
+    response = {
+      status: status,
+      statusText: statusText,
+      text: jest.fn(),
+    }
+  })
+
+  describe('when response body read success', () => {
+    beforeEach(() => {
+      response.text.mockResolvedValue(bodyText)
+      errorView = mount(<ErrorView response={response} />)
+      return response.text()
+        .then(() => errorView = errorView.update())
+    })
+
+    it('have status code', () => {
+      let statusEl = errorView.find('[role="status"]').first()
+      expect(statusEl.text()).toEqual(status.toString())
+    })
+
+    it('have status text', () => {
+      let statusTextEl = errorView.find('[role="statusText"]').first()
+      expect(statusTextEl.text()).toEqual(statusText)
+    })
+
+    it('have body text', () => {
+      let bodyTextEl = errorView.find('[role="bodyText"]').first()
+
+      expect(bodyTextEl.text()).toEqual(bodyText)
+    })
+  })
+
+  describe('when response body read success', () => {
+    let readErrorMessage: string
+    beforeEach(() => {
+      readErrorMessage = lorem.sentence()
+      response.text.mockRejectedValue(new Error(readErrorMessage))
+
+      errorView = mount(<ErrorView response={response} />)
+
+      return new Promise(setImmediate)
+        .then(() => errorView = errorView.update())
+    })
+
+    it('render read error', () => {
+      let bodyTextEl = errorView.find('[role="bodyText"]').first()
+
+      expect(bodyTextEl.text()).toEqual(`Error\n${readErrorMessage}`)
+    })
+  })
+})
---
+++ src/components/ErrorView.tsx
@@ -0,0 +1,70 @@
+import * as React from 'react'
+import withStyles, {
+  StyleRulesCallback,
+  WithStyles,
+} from '@material-ui/core/styles/withStyles'
+import { Typography, Grid } from '@material-ui/core'
+
+const styles: StyleRulesCallback = (theme) => ({
+  container: {
+    flexGrow: 1,
+    height: '100%',
+  },
+  item: {
+    borderStyle: 'solid',
+    borderColor: theme.palette.error.light,
+    padding: theme.spacing.unit * 2,
+  },
+})
+
+export interface ErrorViewProps {
+  response: {
+    status: number,
+    statusText: string,
+    text: () => Promise<string>,
+  },
+}
+
+class State {
+  bodyText?: string
+}
+
+class ErrorView extends React.PureComponent<ErrorViewProps & WithStyles, State> {
+  state = new State()
+
+  render () {
+    return <Grid
+      container
+      justify='center'
+      alignItems='center'
+      className={this.props.classes.container}
+    >
+      <Grid
+        item
+        className={this.props.classes.item}
+      >
+        <Typography variant='headline'>
+          Error
+        </Typography>
+        <Typography variant='subheading'>
+          <span role='status'>{this.props.response.status}</span>:
+          &nbsp;
+          <span role='statusText'>{this.props.response.statusText}</span>
+        </Typography>
+        {this.state.bodyText !== undefined &&
+          <Typography variant='body1' role='bodyText'>
+            {this.state.bodyText}
+          </Typography>}
+      </Grid>
+    </Grid>
+  }
+
+  componentDidMount () {
+    this.props.response.text()
+      .then(bodyText => this.setState({ bodyText }))
+      .catch((error: Error) =>
+        this.setState({ bodyText: `${error.name}\n${error.message}` }))
+  }
+}
+
+export default withStyles(styles)(ErrorView)
