ea5781047b68dd37bccc13aa158ebbb2a84d7daf
Dmitrij Fedorenko <c0va23@gmail.com>
2018-06-30 19:10:23 +0300

    Fix setup fetch package

--- specs/setup.ts
+++ specs/setup.ts
@@ -1,4 +1,7 @@
 import { configure } from 'enzyme'
 import * as ReactSixteenAdapter from 'enzyme-adapter-react-16'
 
+// Import global Response
+import 'isomorphic-fetch'
+
 configure({ adapter: new ReactSixteenAdapter() })
