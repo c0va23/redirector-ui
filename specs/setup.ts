0a574bf7747571d2f6f4bb761523c2b36cdbf21d
Dmitrij Fedorenko <c0va23@gmail.com>
2018-07-01 15:40:35 +0300

    Disable logs for specs

--- specs/setup.ts
+++ specs/setup.ts
@@ -4,4 +4,8 @@ import * as ReactSixteenAdapter from 'enzyme-adapter-react-16'
 // Import global Response
 import 'isomorphic-fetch'
 
+import * as log from 'loglevel'
+
+log.disableAll()
+
 configure({ adapter: new ReactSixteenAdapter() })
